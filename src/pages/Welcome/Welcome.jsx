32ea231c1e4986c7f0fe05d01f3bf42d840550d8
Aman Singh <reachout.amansingh@gmail.com>
2022-10-23 16:54:17 +0530

    feat : welcome page

---
+++ src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./Welcome.css";
+
+export const Welcome = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <>
+      <div className="overlay-text">
+        <h1 className="fs-lg">Hello, what's your name?</h1>
+        <form>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            className="input-box fs-md"
+            type="text"
+          />
+          {name.length > 0 && <button className="submit-btn">Continue</button>}
+        </form>
+      </div>
+    </>
+  );
+};
