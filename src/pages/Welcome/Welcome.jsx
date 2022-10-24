d1071f58dbafbd0e15d60caf618b34db1469ee25
Aman Singh <reachout.amansingh@gmail.com>
2022-10-24 18:20:08 +0530

    feat : add username to localstorage

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -5,18 +5,26 @@ import "./Welcome.css";
 export const Welcome = () => {
   const [name, setName] = useState("");
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    localStorage.setItem("name", name);
+    setName("");
+  };
+
   return (
     <>
       <div className="overlay-text">
         <h1 className="fs-lg">Hello, what's your name?</h1>
-        <form>
+        <form onSubmit={submitHandler}>
           <input
+            required
+            value={name}
             onChange={(e) => setName(e.target.value)}
             className="input-box fs-md"
             type="text"
           />
           {name.length > 0 && (
-            <button className="submit-btn">
+            <button type="submit" className="submit-btn">
               Continue <MdOutlineKeyboardArrowRight />
             </button>
           )}
